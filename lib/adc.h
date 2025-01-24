2398fed64305f045672be817638231f430d719ee
miguel <vackap@gmail.com>
2025-01-23 21:03:29 -0300

    first

---
+++ lib/adc.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Arduino.h>
+
+class Adc {
+
+  public:
+    void initADC();
+
+    void voltage_reference_vcc();
+
+    void voltage_reference_PB0();
+
+    void voltage_reference_internal_1v1();
+
+    void voltage_reference_2v56(bool external_bypass);
+
+    void set_result_shifted(bool left);
+
+    void enable_adc();
+
+    void set_adc_single_ended_input(uint8_t pin_number);
+};
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "adc.h"
+
+
+void setup() {}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
+
+ISR(ADC_vect) {  }
